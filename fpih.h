7f9e5fdd37cc050c5ac94bd85b5f4f77bedbbe98
archiecrewgee <archiecg@hotmail.co.nz>
2026-01-11 14:46:33 +0000

    added saturation functions and tightened up amcros

--- fpih.h
+++ fpih.h
@@ -1,10 +1,15 @@
 #include "stdint.h"
 #include "stddef.h"
+#include "stdio.h"
 
 #define FPIH_DEVELOPMENT
 
+// replace with formated print function with argument order matching stdio's `printf`
+#define fpih_printf(...) printf(__VA_ARGS__)
+
 #define fpih_bit_width(n) (sizeof(n.v) * 8)
-#define fpih_sbit(n) (n.v & ((size_t) 1 << (fpih_bit_width(n) - 1)))
+#define fpih_msb(n) ((size_t) 1 << (fpih_bit_width(n) - 1))
+#define fpih_sbit(n) (n.v & fpih_msb(n))
 
 #define FPIH_32_ONES        (UINT32_MAX)
 
@@ -15,6 +20,12 @@ typedef struct {
 
 typedef ufpt32_t fpt32_t;
 
+/* util functions (32-bit) */
+static inline uint32_t fpih_32_logical_lshift(uint32_t n, size_t shift);
+static inline uint32_t fpih_32_logical_rshift(uint32_t n, size_t shift);
+static inline uint32_t fpih_32_msb_index(uint32_t n);
+static inline uint32_t fpih_32_lsb_index(uint32_t n);
+
 /**
  * @brief performs a logical left shift on a 32-bit value
  * 
@@ -23,6 +34,10 @@ typedef ufpt32_t fpt32_t;
  * @return uint32_t `n << shift`
  */
 static inline uint32_t fpih_32_logical_lshift(uint32_t n, size_t shift) {
+    #ifdef FPIH_DEVELOPMENT
+    if (shift > (sizeof(n) * 8) - fpih_32_msb_index(n)) fpih_printf("[FPIH] overflow warning @ %s: %u << %u\n", __FUNCTION_NAME__, n, shift);
+    #endif
+
     n <<= shift;
     return n; 
 }
@@ -35,8 +50,38 @@ static inline uint32_t fpih_32_logical_lshift(uint32_t n, size_t shift) {
  * @return uint32_t `n >> shift`
  */
 static inline uint32_t fpih_32_logical_rshift(uint32_t n, size_t shift) {
+    #ifdef FPIH_DEVELOPMENT
+    if (shift > fpih_32_lsb_index(n)) fpih_printf("[FPIH] underflow warning @ %s: %u >> %u\n", __FUNCTION_NAME__, n, shift);
+    #endif
+
     n >>= shift;
     return n; 
+}
+
+/**
+ * @brief returns the index of the most significant bit of a 32-bit value; for example `fpih_32_msb_index(0x000008f8) == 11`
+ * 
+ * @param n 32-bit value
+ * @return uint32_t MSB index
+ */
+static inline uint32_t fpih_32_msb_index(uint32_t n) {
+    uint32_t index = sizeof(n) - 1;
+    for (uint32_t v = 1 << (sizeof(v) - 1); v > 0; v = fpih_32_logical_rshift(v, 1), index--) 
+        if (v & n) break;
+    return index;
+}
+
+/**
+ * @brief returns the index of the least significant bit of a 32-bit value; for example `fpih_32_lsb_index(0x000008f8) == 3`
+ * 
+ * @param n 32-bit value
+ * @return uint32_t LSB index
+ */
+static inline uint32_t fpih_32_lsb_index(uint32_t n) {
+    uint32_t index = 0;
+    for (uint32_t v = 1; v > 0; v = fpih_32_logical_lshift(v, 1), index++) 
+        if (v & n) break;
+    return index;
 }
 
 /**
